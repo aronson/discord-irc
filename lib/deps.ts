004d9b3a1e760c5bfd7962437bde1581dfda36ab
aronson <i@pingas.org>
2023-10-01 16:08:56 -0500

    Wrap all IRC handlers in try-catch for stability (#20)
    * Wrap all IRC handlers in try-catch for stability
    
    * Update IRC library
    
    * Revert "Wrap all IRC handlers in try-catch for stability"
    
    This reverts commit 4f584cb9b5c480dc1b668f5a1c8385e5387b7554.
    
    * Format README

--- lib/deps.ts
+++ lib/deps.ts
@@ -1,17 +1,17 @@
 // IRC exports
-export { Client as IrcClient } from 'https://deno.land/x/irc@v0.12.5/mod.ts';
-export type { ClientOptions } from 'https://deno.land/x/irc@v0.12.5/mod.ts';
-export type { PrivmsgEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/privmsg.ts';
-export type { ClientError } from 'https://deno.land/x/irc@v0.12.5/core/errors.ts';
-export type { RegisterEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/register.ts';
-export type { NoticeEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/notice.ts';
-export type { NickEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/nick.ts';
-export type { JoinEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/join.ts';
-export type { PartEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/part.ts';
-export type { QuitEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/quit.ts';
-export type { NicklistEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/nicklist.ts';
-export type { CtcpActionEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/action.ts';
-export type { InviteEvent } from 'https://deno.land/x/irc@v0.12.5/plugins/invite.ts';
+export { Client as IrcClient } from 'https://deno.land/x/irc@v0.14.0/mod.ts';
+export type { ClientOptions } from 'https://deno.land/x/irc@v0.14.0/mod.ts';
+export type { PrivmsgEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/privmsg.ts';
+export type { ClientError } from 'https://deno.land/x/irc@v0.14.0/core/errors.ts';
+export type { RegisterEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/register.ts';
+export type { NoticeEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/notice.ts';
+export type { NickEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/nick.ts';
+export type { JoinEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/join.ts';
+export type { PartEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/part.ts';
+export type { QuitEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/quit.ts';
+export type { NicklistEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/nicklist.ts';
+export type { CtcpActionEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/action.ts';
+export type { InviteEvent } from 'https://deno.land/x/irc@v0.14.0/plugins/invite.ts';
 // Harmony/Discord exports
 export {
   AllowedMentionType,
