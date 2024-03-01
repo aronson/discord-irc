6bccb0f6d54af5ed30a3072507678a70b2ebd86a
tuhana <git@tuhana.me>
2024-03-02 01:48:37 +0300

    Pin harmony to a commit before decorators update

--- lib/deps.ts
+++ lib/deps.ts
@@ -15,12 +15,14 @@ export type { CtcpActionEvent } from 'https://deno.land/x/irc@v0.15.0/plugins/ac
 export type { InviteEvent } from 'https://deno.land/x/irc@v0.15.0/plugins/invite.ts';
 export type { AnyRawCommand } from 'https://deno.land/x/irc@v0.15.0/core/protocol.ts';
 export type { CtcpVersionEvent } from 'https://deno.land/x/irc@v0.15.0/plugins/version.ts';
+
 // Harmony/Discord exports
 export {
   AllowedMentionType,
   Client,
   Command,
   CommandClient,
+  type CommandContext,
   DiscordAPIError,
   event,
   GatewayIntents,
@@ -34,29 +36,33 @@ export {
   Role,
   slash,
   SlashCommandOptionType,
+  type SlashCommandPartial,
   User,
   Webhook,
-} from 'https://raw.githubusercontent.com/harmonyland/harmony/main/mod.ts';
-export type {
-  CommandContext,
-  SlashCommandPartial,
-} from 'https://raw.githubusercontent.com/harmonyland/harmony/main/mod.ts';
-export type { AllWebhookMessageOptions } from 'https://raw.githubusercontent.com/harmonyland/harmony/main/src/structures/webhook.ts';
+} from 'https://raw.githubusercontent.com/harmonyland/harmony/1821fc19428def11f468102631b9c53f37036f0b/mod.ts';
+export type { AllWebhookMessageOptions } from 'https://raw.githubusercontent.com/harmonyland/harmony/1821fc19428def11f468102631b9c53f37036f0b/src/structures/webhook.ts';
+
 // std exports
 export { resolve as resolvePath } from 'https://deno.land/std@0.203.0/path/mod.ts';
 export { parse as parseCLI } from 'https://deno.land/std@0.203.0/flags/mod.ts';
 export { parse as parseJSONC } from 'https://deno.land/std@0.203.0/jsonc/mod.ts';
+
 // Logging
 import Dlog from 'https://deno.land/x/dlog2@2.0/classic.ts';
 export { Dlog };
+
 // PluralKit support
 export { APIError, Member as PKMember, PKAPI } from 'https://deno.land/x/pkapi@v6.0.1/lib/mod.ts';
+
 // Queue
 export { Queue } from 'https://deno.land/x/queue@1.2.0/mod.ts';
+
 // Time to Live cache
 import TTL from 'https://deno.land/x/ttl@1.0.1/mod.ts';
 export { TTL };
+
 // Event handler
 export { Reflect } from 'https://deno.land/x/reflect_metadata@v0.1.12/mod.ts';
+
 // Regex escape
 export { escapeStringRegexp } from 'https://deno.land/x/escape_string_regexp@v0.0.1/mod.ts';
